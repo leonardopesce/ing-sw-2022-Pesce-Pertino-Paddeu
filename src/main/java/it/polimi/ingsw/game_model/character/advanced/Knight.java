5ffdb8a2de8ec65e156d0a4bcdca2a028636eb01
Alberto Paddeu <alberto.paddeu@mail.polimi.it>
2022-03-26 13:26:34 +0100

    Update game model logic and clean up

--- src/main/java/it/polimi/ingsw/game_model/character/advanced/Knight.java
+++ src/main/java/it/polimi/ingsw/game_model/character/advanced/Knight.java
@@ -22,4 +22,8 @@ public class Knight {
                 }
         );
     }
+
+    public void resetEffect(){
+        game.setInfluenceCalculator(new CalculatorInfluence());
+    }
 }
--- src/main/java/it/polimi/ingsw/game_model/character/advanced/Monk.java
+++ src/main/java/it/polimi/ingsw/game_model/character/advanced/Monk.java
@@ -1,28 +1,31 @@
-package it.polimi.ingsw.game_model.character.advanced.PropostaDiPaolo;
+package it.polimi.ingsw.game_model.character.advanced;
 
 import it.polimi.ingsw.custom_exceptions.BagEmptyException;
 import it.polimi.ingsw.game_model.character.basic.Student;
+import it.polimi.ingsw.game_model.character.character_utils.AdvancedCharacterType;
 import it.polimi.ingsw.game_model.game_type.Game;
 import it.polimi.ingsw.game_model.world.Island;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Bard {
-    private final Game game;
+public class Bard extends AdvancedCharacter{
     private final List<Student> studentsOnCard;
 
     public Bard(Game game) {
-        this.game = game;
+        super(AdvancedCharacterType.BARD, game);
         this.studentsOnCard = new ArrayList<>();
 
         // Setting up 4 students on the card
         studentsOnCard.addAll(game.getBag().drawNStudentFromBag(4));
     }
 
-    public void playEffect(Island islandToPlaceStudentOn, int studentToPick) {
+
+
+    public void playEffect(Island islandToPlaceStudentOn, Integer studentToPick) {
+        //game.playerMoveStudentToIsland();
         // Adding the selected student to the selected island
-        islandToPlaceStudentOn.addStudent(studentsOnCard.remove(studentToPick));
+        islandToPlaceStudentOn.addStudent(studentsOnCard.remove(studentToPick.intValue()));
 
         // picking up a new student from the bag and putting it on the card
         try {
