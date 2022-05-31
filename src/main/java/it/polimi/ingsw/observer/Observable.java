6c5b3133b25b74098be64dc2ad622ec1af69c466
Alberto Paddeu <alberto.paddeu@mail.polimi.it>
2022-05-31 18:08:57 +0200

    [JavaDoc]
    Observer, observable

--- src/main/java/it/polimi/ingsw/observer/Observable.java
+++ src/main/java/it/polimi/ingsw/observer/Observable.java
@@ -3,22 +3,37 @@ package it.polimi.ingsw.observer;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Object that trigger itself when the object <code>Observer</code> gets an update
+ */
 public class Observable<T> {
 
     private final List<Observer<T>> observers = new ArrayList<>();
 
+    /**
+     * given a list of observers, add an observer to the list
+     * @param observer list of observers
+     */
     public void addObserver(Observer<T> observer){
         synchronized (observers) {
             observers.add(observer);
         }
     }
 
+    /**
+     * given a list of observers, remove an observer from the list
+     * @param observer list of observers
+     */
     public void removeObserver(Observer<T> observer){
         synchronized (observers) {
             observers.remove(observer);
         }
     }
 
+    /**
+     * Trigger the update on every server present in the list.
+     * @param message the object that triggers the update
+     */
     protected void notify(T message){
         synchronized (observers) {
             for(Observer<T> observer : observers){
--- src/main/java/it/polimi/ingsw/observer/Observer.java
+++ src/main/java/it/polimi/ingsw/observer/Observer.java
@@ -1,5 +1,8 @@
 package it.polimi.ingsw.observer;
 
+/**
+ * Object that notifies the observers when it gets an update.
+ */
 public interface Observer<T> {
     void update(T message);
 }
